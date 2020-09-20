f7e618266022591dbbd28f09ca17cf82e793b5a6
Zhenlei Ji <zhenlei.ji@gmail.com>
2020-09-20 08:10:23 +0200

    Set up jacoco (#11)

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -13,10 +13,12 @@ object BuildPlugins {
         const val kotlinAndroid = "kotlin-android"
         const val ktlintPlugin = "org.jlleitschuh.gradle.ktlint"
         const val detektPlugin = "io.gitlab.arturbosch.detekt"
+        const val jacocoPlugin = "com.vanniktech.android.junit.jacoco"
     }
 
     object Versions {
         const val ktlintPlugin = "9.4.0"
         const val detektPlugin = "1.13.1"
+        const val jacocoPlugin = "0.16.0"
     }
 }
